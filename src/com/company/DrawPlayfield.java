845e032ae317d64eda45fe96df21dc12564ae629
BNiklas <niklas.burman@hotmail.se>
2018-02-22 15:03:56 +0100

    Added playarea and controls

--- src/com/company/DrawPlayfield.java
+++ src/com/company/DrawPlayfield.java
@@ -12,6 +12,23 @@ public class DrawPlayfield {
     // write controls
 
     public void drawPlay(Terminal terminal, Player player, Monster[] monsters) {
+        for(int i=0; i<RunGame.PLAY_AREA_HEIGHT) {
+            terminal.moveCursor(RunGame.PLAY_AREA_WIDTH, i);
+            terminal.putCharacter('|');
+        }
+        for(int i=0; i<=RunGame.PLAY_AREA_WIDTH; i++){
+            terminal.moveCursor(i,RunGame.PLAY_AREA_HEIGHT);
+            terminal.putCharacter('-');
+        }
+
+        String controls = "Controls:\n\tArrow Up = move up\n\tArrow Down = move down" +
+                "\n\tArrow left = move left\n\tArrow Right = move right";
+        char[] c = controls.toCharArray();
+        terminal.moveCursor(30,5);
+        for(char i:c) {
+            terminal.putCharacter(i);
+        }
+
         terminal.moveCursor(player.xPlayer, player.yPlayer);
         terminal.putCharacter('O');
 
