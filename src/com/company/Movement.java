6dda5d424c07fc3c8dee0a9eb8a969b12b8b3888
Sofie Westerdahl <algotson@gmail.com>
2018-02-22 10:32:00 +0100

    Added classes

---
+++ src/com/company/Movement.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import com.googlecode.lanterna.*;
+import com.googlecode.lanterna.input.Key;
+import com.googlecode.lanterna.terminal.Terminal;
+import java.nio.charset.Charset;
+
+public class Movement {
+    //Movement
+    //   -button press
+    //   -player move
+    //   -monster move
+}
